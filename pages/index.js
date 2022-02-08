845b16a12b046f05d82277dcfb8809bf2c7fef10
Lovely <yaroslavgaponenko04041996@gmail.com>
2022-02-08 20:33:31 +0900

    modify logo_ori

--- pages/index.js
+++ pages/index.js
@@ -42,7 +42,7 @@ export default function Home() {
         <meta name="twitter:image" content="https://mint.theartofori.com/images/Hola.gif" key="twimage" />
       </Head>
       <div className="flex items-center custom-text justify-between w-full p-3">
-        <Link href="/" className=""><img src="images/mark.png" width="108" alt="TheArtOfOri" className="logo-image" /></Link>
+        <Link href="/" className=""><img src="images/logo_ori.png" width="108" alt="TheArtOfOri" className="logo-image" /></Link>
         <nav className="flex flex-wrap flex-row justify-around">
           <Link href="/mint" className="custom-menu text-white hover:text-slate-100 m-3 sm:m-6 hidden md:block">
             MINT YOUR BADGE
