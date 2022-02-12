c83a8c10508470618fd8d4792693b35aeadce808
Lovely <yaroslavgaponenko04041996@gmail.com>
2022-02-12 14:47:10 +0900

    revert: background image, deleted: temp images

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,8 @@ module.exports = {
   theme: {
     extend: { 
       backgroundImage: {
-        'mint-page-pattern': "url('/images/2.jpg')",
-        'main-page-pattern': "url('/images/bg_landing.webp')",
+        'mint-page-pattern': "url('/images/bg_landing.jpg')",
+        'main-page-pattern': "url('/images/bg_landing.jpg')",
        }
     },
   },
